c1ec1d2525f749ea628b784a5178cf6b90993213
Fourth1755 <87474795+Fourth1755@users.noreply.github.com>
2022-04-27 14:08:17 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
-
+import Navbar from "./component/Navbar";
 const App=()=> {
   return (
     <div>
+        
         <h1>Hello</h1>
     </div>
   );
