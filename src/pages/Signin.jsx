f8d0069e44930efebf15927b8f03409d40e244be
Fourth1755 <87474795+Fourth1755@users.noreply.github.com>
2022-05-04 14:21:22 +0700

    Update Signin.jsx

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import {useDispatch} from 'react-redux'
-
+import {fetchAuthAsync} from '../actions/authActions'
 
 const Signin=()=>{
-    const [username,setUsername]=useState('')
+    const [email,setEmail]=useState('')
     const [password,setPassword]=useState('')
 
     const dispatch=useDispatch()
@@ -13,13 +13,13 @@ const Signin=()=>{
             <form>
                 <div class="mb-3">
                     <label class="form-label">Email address</label>
-                    <input type="email" class="form-control" value={username} onChange={e=>setUsername(e.target.value)} placeholder="name@example.com"/>
+                    <input type="email" class="form-control" value={email} onChange={e=>setEmail(e.target.value)} placeholder="name@example.com"/>
                 </div>
                 <div class="mb-3">
                     <label class="form-label">Password</label>
                     <input type="password" class="form-control" value={password} onChange={e=>setPassword(e.target.value)} placeholder=""/>
                 </div>
-                <button onClick={dispatch()}>Submit</button>    
+                <button onClick={dispatch(fetchAuthAsync(email,password))}>Submit</button>    
             </form>
         </div>
     );
