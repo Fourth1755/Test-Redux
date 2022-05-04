84718ce073f94c4791cb7c26ff289fc4461bf84e
Fourth1755 <87474795+Fourth1755@users.noreply.github.com>
2022-05-04 14:21:25 +0700

    Update ItemAnime.jsx

--- src/component/ItemAnime.jsx
+++ src/component/ItemAnime.jsx
@@ -1,15 +1,25 @@
 import React, { useState, useEffect } from "react";
-import {useDispatch} from 'react-redux'
+import {useDispatch,useSelector} from 'react-redux'
 import { addToList } from "../actions/animeListActions";
+import { useHistory } from "react-router-dom";
 const ItemAnime =({anime})=>{
     const dispatch=useDispatch()
+    const {user} =useSelector(state=>state.auth)
+    const history= useHistory()
+    function handleAddToList(){
+        if(!user){
+            history.push('/signin')
+        }else{
+            dispatch(addToList({...anime,quantity:1}))
+        }
+    }
     return(
         <div className="card" style={{width: '14rem',margin: '20px'}}>
             <img className="card-img-top" src={anime.image} alt="Card image cap"/>
             <div className="card-body">
                 <h5 className="card-title">{anime.name}</h5>
                 <p className="card-text">{anime.year}</p>
-                <button onClick={()=>dispatch(addToList({...anime,quantity:1}))} className="btn btn-primary">Add to list</button>
+                <button onClick={()=>handleAddToList()} className="btn btn-primary">Add to list</button>
             </div>
         </div>
     );
