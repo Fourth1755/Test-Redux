815a556c9347061811fe6ed692f76bf23339a787
Fourth1755 <87474795+Fourth1755@users.noreply.github.com>
2022-05-04 11:57:32 +0700

    update

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,8 +1,29 @@
+import {startFetch,endFetch,errorFetch} from '../actions/statusAction'
+import {signin} from '../api/user'
 export const SET_AUTH='SET_AUTH'
 
 export function setAuth(user){
     return{
         type:SET_AUTH,
         payload:user
+    }
+}
+export function fetchAuthAsync(email,password){
+    return async function(dispatch){
+        try{
+            dispatch(startFetch())
+
+            const user = await signin(email,password)
+
+            if(user){
+                dispatch(setAuth(user))
+                dispatch(errorFetch(''))
+                dispatch(endFetch())
+            }
+        }catch(error){
+            dispatch(setAuth(null))
+            dispatch(errorFetch(error))
+            dispatch(endFetch())
+        }
     }
 }
--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,11 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import {useSelector} from 'react-redux'
+import {useDispatch} from 'react-redux'
+
 
 const Signin=()=>{
-    return(
-        <div>
+    const [username,setUsername]=useState('')
+    const [password,setPassword]=useState('')
 
+    const dispatch=useDispatch()
+    return(
+        <div className="container">
+            <form>
+                <div class="mb-3">
+                    <label class="form-label">Email address</label>
+                    <input type="email" class="form-control" value={username} onChange={e=>setUsername(e.target.value)} placeholder="name@example.com"/>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Password</label>
+                    <input type="password" class="form-control" value={password} onChange={e=>setPassword(e.target.value)} placeholder=""/>
+                </div>
+                <button onClick={dispatch()}>Submit</button>    
+            </form>
         </div>
     );
 }
